04c53c0d4d80f25b3f8988adfceeb4ae1a58867a
Anita <orjianita15@gmail.com>
2022-06-13 06:43:44 -0700

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints all possible combinations of three different digits,
- * 	  in ascending order, seperated by a comma followed by a space.
+ * 	in ascending order, seperated by a comma followed by a space.
  *
  * Return: Always 0.
  */
@@ -29,6 +29,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
