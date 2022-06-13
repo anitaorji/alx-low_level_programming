93dcf491b214ab7c721d95c4ba5074ef743ea249
Anita <orjianita15@gmail.com>
2022-06-13 07:01:42 -0700

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two different digits,
- * 	in ascending order, separated by a comma followed by a space.
+ * main - Print different two digits
  *
- * 	Return: Always 0.
+ *	Return: Always 0.
  */
 int main(void)
 {
@@ -25,6 +24,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
