683e1a4d3b58a22ebe0b10dc6b1d0206a5b3b69a
Anita <orjianita15@gmail.com>
2022-06-13 05:57:06 -0700

    correct

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, except for q and e. 
+ * main - Print alphabet except q and e 
  *
  * Return: Always 0.
  */
@@ -18,4 +18,3 @@ int main(void)
 
 	return (0);
 }
-
