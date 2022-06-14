b10f05b6bf655650bbaeb05640d989206f8aaf9a
Anita <orjianita15@gmail.com>
2022-06-14 03:11:04 -0700

    mustbe

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 
 /*
  * main - write the character
@@ -9,14 +10,7 @@
  */
 int main(void)
 {
-	putchar('-');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	write (1, "_putchar\n" ,9);
+
 	return (0);
 }
